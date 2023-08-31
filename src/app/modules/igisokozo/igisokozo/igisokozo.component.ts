fbbbdcfd0fcafa5da9d36ded2fb2ff0739ab6f50
dondelice257 <dondelicedushime@gmail.com>
2023-09-01 01:06:08 +0200

    add ios

--- src/app/modules/igisokozo/igisokozo/igisokozo.component.ts
+++ src/app/modules/igisokozo/igisokozo/igisokozo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-igisokozo',
@@ -15,6 +15,12 @@ export class IgisokozoComponent {
   hasGivenUp:boolean=false;
   providedAnswer:string='';
   answerDisabled:boolean=false;
+  @ViewChild('correctAudioElement', { static: false }) correctAudioElement: ElementRef | any;
+  @ViewChild('incorrectAudioElement', { static: false }) incorrectAudioElement: ElementRef |any;
+  correctAnswerImageVisible: boolean = false;
+  animateImage: boolean = false;
+  score:number=0
+
 
   ibisokozo=[
     {
@@ -82,7 +88,153 @@ export class IgisokozoComponent {
 
 
       ]
-    }
+    },
+    {
+      igisokozo:'Icutabonye', 
+      answers:[
+        {answer:"Ivuka ryawe", isCorrect:false},
+        {answer:"Umwungu womurubibe", isCorrect:false},
+        {answer:"Ubugeni bwaso na nyoko", isCorrect:true},
+        {answer:"Akari munda y'ingoma", isCorrect:false},
+
+
+      ]
+    },
+     {
+      igisokozo:'Abatama bakera baca imanza bunamye', 
+      answers:[
+        {answer:"Umugabo yicisha bugufi", isCorrect:false},
+        {answer:"Imigondoro y'ibiharage", isCorrect:true},
+        {answer:"Igiti kigoramye", isCorrect:false},
+        {answer:"Intama", isCorrect:false},
+
+
+      ]
+    },
+     {
+      igisokozo:'Abatama bakera baca imanza bunamye', 
+      answers:[
+        {answer:"Umugabo yicisha bugufi", isCorrect:false},
+        {answer:"Imigondoro y'ibiharage", isCorrect:true},
+        {answer:"Igiti kigoramye", isCorrect:false},
+        {answer:"Intama", isCorrect:false},
+
+
+      ]
+    },
+    {
+      igisokozo:'Ndakibona singifata', 
+      answers:[
+        {answer:"Inzahabu", isCorrect:false},
+        {answer:"Igitutu", isCorrect:true},
+        {answer:"Icontari buronke", isCorrect:false},
+        {answer:"Umuyaga", isCorrect:false},
+
+
+      ]
+    },
+    {
+      igisokozo:'Umwana wanje yokunesha ari muto', 
+      answers:[
+        {answer:"Ivubi", isCorrect:false},
+        {answer:"Akanyegeri", isCorrect:false},
+        {answer:"Ifundi", isCorrect:false},
+        {answer:"Uruyuki", isCorrect:true},
+
+
+      ]
+    },
+    {
+      igisokozo:'Abana banje bakubita ibiganza hirya no hino', 
+      answers:[
+        {answer:"Intoke", isCorrect:false},
+        {answer:"Abatamvyi", isCorrect:false},
+        {answer:"Inzuzi", isCorrect:true},
+        {answer:"Amashami y'igiti", isCorrect:false},
+
+
+      ]
+    },
+    {
+      igisokozo:'Abana banje bakubita ibiganza hirya no hino', 
+      answers:[
+        {answer:"Intoke", isCorrect:false},
+        {answer:"Abatamvyi", isCorrect:false},
+        {answer:"Inzuzi", isCorrect:true},
+        {answer:"Amashami y'igiti", isCorrect:false},
+
+
+      ]
+    },
+     {
+      igisokozo:'Uhmmm', 
+      answers:[
+        {answer:"Igikamiwe make", isCorrect:true},
+        {answer:"Inzoka munda", isCorrect:false},
+        {answer:"Umugabo yemeye atemeye", isCorrect:false},
+        {answer:"Ikidumu kigaragara", isCorrect:false},
+
+
+      ]
+    },
+       {
+      igisokozo:'Aha twese turaririye umwe', 
+      answers:[
+        {answer:"Umuvyeyi wacu", isCorrect:false},
+        {answer:"Inkono kuziko", isCorrect:true},
+        {answer:"Umwami", isCorrect:false},
+        {answer:"Ingabo z'umukuru w'igihugu", isCorrect:false},
+
+
+      ]
+    },
+           {
+      igisokozo:"Wigera mubarembe war'umurembe ?", 
+      answers:[
+        {answer:"Umuzungu mubirabure", isCorrect:false},
+        {answer:"Inka yera muzirabura", isCorrect:false},
+        {answer:"Impene muntama", isCorrect:true},
+        {answer:"Intama munka", isCorrect:false},
+
+
+      ]
+    },
+         {
+      igisokozo:"Abo kwa Ruringaniza bambariye imiringa mu kuzimu.", 
+      answers:[
+        {answer:"Ibijumbu", isCorrect:true},
+        {answer:"Imfuku", isCorrect:false},
+        {answer:"Imyumbati", isCorrect:false},
+        {answer:"Iyo hasi", isCorrect:false},
+
+
+      ]
+    },
+      {
+      igisokozo:"Karahinda iduri", 
+      answers:[
+        {answer:"Akana gakubiswe", isCorrect:false},
+        {answer:"Akato mumpange", isCorrect:true},
+        {answer:"Inanga", isCorrect:false},
+        {answer:"Uruhwa", isCorrect:false},
+
+
+      ]
+    },
+      {
+      igisokozo:"Urabona ngo bitugu vya sogokuru ansimbira urugo!", 
+      answers:[
+        {answer:"Umugabo asumba urugo", isCorrect:false},
+        {answer:"Umwana asumba se", isCorrect:false},
+        {answer:"Ikivumvuri", isCorrect:true},
+        {answer:"Igifyera", isCorrect:false},
+
+
+      ]
+    },
+    
+
+    
   ]
 
   ngOnInit(){
@@ -92,10 +244,12 @@ export class IgisokozoComponent {
     this.answerDisabled=false
     for(let i=0; i<this.ibisokozo.length; i++){
       this.igisokozo=this.ibisokozo[Math.floor(Math.random() * this.ibisokozo.length)]
+
+      console.log(this.ibisokozo.length)
+    this.closeModal();
+
     }
 
-
-    this.closeModal();
     
     this.hasGivenUp=false
     
@@ -105,9 +259,27 @@ export class IgisokozoComponent {
     this.openModal();
     this.isCorrect = item.isCorrect;
     this.providedAnswer=item.answer
+
+    if (this.isCorrect) {
+      this.playCorrectAudio(); 
+      this.score++
+    } else {
+      this.playIncorrectAudio(); 
+    }
+    
       
   
   }
+
+getIgisokozo2(){
+  for(let i=0; i<this.ibisokozo.length; i++){
+    this.igisokozo=this.ibisokozo[Math.floor(Math.random() * this.ibisokozo.length)]
+  this.closeModal();
+
+  }
+
+}
+
   
 
   giveUp(){
@@ -139,6 +311,24 @@ closeModal(){
 
 }
 
+
+playCorrectAudio() {
+  const audio: HTMLAudioElement = this.correctAudioElement.nativeElement;
+  audio.play();
+
+  this.correctAnswerImageVisible = true;
+  this.animateImage=true
+
+  setTimeout(() => {
+    this.correctAnswerImageVisible = false;
+    this.animateImage=false
+  }, 2000);
+}
+
+playIncorrectAudio() {
+  const audio: HTMLAudioElement = this.incorrectAudioElement.nativeElement;
+  audio.play();
+}
 
 
 
