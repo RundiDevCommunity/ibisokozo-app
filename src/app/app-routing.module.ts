4bd1fac93bed9f83426e847cf792008222717a36
dondelice257 <dondelicedushime@gmail.com>
2023-11-18 00:05:30 +0200

    correct routing issues on android

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -49,7 +49,7 @@ children:[
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true}),],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
