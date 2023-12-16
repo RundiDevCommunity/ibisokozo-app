d51aeeb1a1120fc67d69ece1cd5feb656b4d3ec7
dondelice257 <dondelicedushime@gmail.com>
2023-12-16 15:22:47 +0200

    redesign the signup page

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -5,13 +5,15 @@ import { SignupComponent } from './signup/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { LayoutModule } from '../layout/layout.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
     SharedModule,
-    LayoutModule
+    LayoutModule,
+    RouterModule
   ],
   declarations: [
     LoginComponent,
--- src/app/modules/igisokozo/level-two/level-two.component.ts
+++ src/app/modules/igisokozo/level-two/level-two.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LevelTwoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _location:Location
+  ) { }
 
   ngOnInit() {
+  }
+
+  goBack(){
+    this._location.back()
   }
 
 }
